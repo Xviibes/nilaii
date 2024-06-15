69b0306b81304ef8582d2a8e35a84ab33c9c4486
Xviibes <134538998+Xviibes@users.noreply.github.com>
2024-06-15 08:01:44 +0400

    update.php

--- mapel/update.php
+++ mapel/update.php
@@ -5,12 +5,12 @@ if (isset($_POST['update'])) {
     $mapel = $_POST['mapel'];
     $sks = $_POST['sks'];
 
-    $sql = "UPDATE mapel SET mapel='$mapel', sks='$sks'";
+    $sql = "UPDATE mapel SET mapel='$mapel', sks='$sks' WHERE id='$id'";
     $result = mysqli_query($con, $sql);
     if ($result) {
         header('location: index.php?m=mapel&s=view');
     } else {
-        include("index.php?m=mapel");
+        include("index.php?m=mapel&s=view");
         echo '<script language="JavaScript">';
         echo 'alert("Data Gagal Ditambahkan.")';
         echo '</script>';
